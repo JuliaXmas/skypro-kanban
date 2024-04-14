12a5cf7e2176dc542cfcf8406c6c66924ef38302
JuliaXmas <julia069910@gmail.com>
2024-04-14 16:55:24 +0300

    Decompose to components header and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/main";
 
 function App() {
   return (
@@ -401,47 +403,49 @@ function App() {
           </div>
         </div>
       </div>
-
-      <header className="header">
-        <div className="container">
-          <div className="header__block">
-            <div className="header__logo _show _light">
-              <a href="" target="_self">
-                <img src="./images/logo.png" alt="logo" />
-              </a>
-            </div>
-            <div className="header__logo _dark">
-              <a href="" target="_self">
-                <img src="./images/logo_dark.png" alt="logo" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <button className="header__btn-main-new _hover01" id="btnMainNew">
-                <a href="#popNewCard">Создать новую задачу</a>
-              </button>
-              <a href="#user-set-target" className="header__user _hover02">
-                Ivan Ivanov
-              </a>
-              <div
-                className="header__pop-user-set pop-user-set"
-                id="user-set-target"
-              >
-                {/*  <!-- <a href="">x</a> --> */}
-                <p className="pop-user-set__name">Ivan Ivanov</p>
-                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                <div className="pop-user-set__theme">
-                  <p>Темная тема</p>
-                  <input type="checkbox" className="checkbox" name="checkbox" />
-                </div>
-                <button type="button" className="_hover03">
-                  <a href="#popExit">Выйти</a>
-                </button>
-              </div>
-            </nav>
+      <Header />
+    {/* <header className="header">
+      <div className="container">
+        <div className="header__block">
+          <div className="header__logo _show _light">
+            <a href="" target="_self">
+              <img src="./images/logo.png" alt="logo" />
+            </a>
           </div>
+          <div className="header__logo _dark">
+            <a href="" target="_self">
+              <img src="./images/logo_dark.png" alt="logo" />
+            </a>
+          </div>
+          <nav className="header__nav">
+            <button className="header__btn-main-new _hover01" id="btnMainNew">
+              <a href="#popNewCard">Создать новую задачу</a>
+            </button>
+            <a href="#user-set-target" className="header__user _hover02">
+              Ivan Ivanov
+            </a>
+            <div
+              className="header__pop-user-set pop-user-set"
+              id="user-set-target"
+            >
+
+              <p className="pop-user-set__name">Ivan Ivanov</p>
+              <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+              <div className="pop-user-set__theme">
+                <p>Темная тема</p>
+                <input type="checkbox" className="checkbox" name="checkbox" />
+              </div>
+              <button type="button" className="_hover03">
+                <a href="#popExit">Выйти</a>
+              </button>
+            </div>
+          </nav>
         </div>
-      </header>
-      <main className="main">
+      </div>
+    </header> */}
+
+      <Main />
+      {/* <main className="main">
         <div className="container">
           <div className="main__block">
             <div className="main__content">
@@ -1061,7 +1065,7 @@ function App() {
             </div>
           </div>
         </div>
-      </main>
+      </main> */}
     </div>
   );
 }
